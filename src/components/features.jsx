84223a118fd4e7036023568651d505032bf4a5a0
notMrpod <darsh.pod@gmail.com>
2023-07-18 21:10:19 -0500

    Feature icon interact using scale() CSS method

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -11,8 +11,9 @@ export const Features = (props) => {
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={d.icon}></i>
+                  <button className="icon-button">
+                    <i className={d.icon}></i>
+                  </button>
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                 </div>
